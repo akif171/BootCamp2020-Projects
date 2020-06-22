7ac75e4321320fe364d7390b5781372830d5be66
Muhammad Akif <57750292+akif171@users.noreply.github.com>
2020-06-23 00:00:03 +0500

    Add files via upload

---
+++ build/precache-manifest.a7eba2178019ff9e03086e4c6bd46e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059c224853482ceb52a6abb63e618d5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfdc58a0418fd408bb8b",
+    "url": "/static/css/main.2574ccca.chunk.css"
+  },
+  {
+    "revision": "07e4aa8bb2a0b9bb4bcb",
+    "url": "/static/js/2.138ff683.chunk.js"
+  },
+  {
+    "revision": "e46fc7d39878ee64e7b083a156855138",
+    "url": "/static/js/2.138ff683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfdc58a0418fd408bb8b",
+    "url": "/static/js/main.05399e55.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
